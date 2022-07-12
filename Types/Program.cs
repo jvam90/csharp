0afd6df8ecd1303aa4409d6c8891dcc28b2fe844
jvam90 <jvam90@CASA>
2022-07-11 21:36:11 -0300

    Adicionar arquivos de projeto.

---
+++ Types/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Types
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 32;
+            Int32 alsoAge = 32;
+
+            char c = 'J';
+            Char alsoC = 'J';
+
+            byte b = 0;
+            Byte alsoB = 1;
+
+            Console.WriteLine("Int32 == int");
+            Console.WriteLine(age);
+            Console.WriteLine(alsoAge);
+            Console.WriteLine("Char == char");
+            Console.WriteLine(c);
+            Console.WriteLine(alsoC);
+            Console.WriteLine("Byte == byte");
+            Console.WriteLine(b);
+            Console.WriteLine(alsoB);
+        }
+    }
+}
