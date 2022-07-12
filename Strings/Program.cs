1b6fc0cbee28bc5f74a212308d2bdf2c6c2e2565
jvam90 <jvam90@gmail.com>
2022-07-12 01:22:21 -0300

    Strings lesson.

---
+++ Strings/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Joao Victor Alves Martins";
+            string anotherString = new string('a', 2);
+
+            string bar = "John\\bar";
+            string newLine = "John\nNew Line";
+
+            string unicodeExample = "\u03a3";
+
+            string verbatim = @"C:\Users\jvam90\source\repos\CSharpStuff";
+
+            //Console.WriteLine(name);
+            //Console.WriteLine(anotherString);
+            //Console.WriteLine(bar);
+            //Console.WriteLine(newLine);
+            //Console.WriteLine(unicodeExample);
+            Console.WriteLine(verbatim);
+
+        }
+    }
+}
