1b18a85f151fb98b3917fcf268345840a4c14a61
jvam90 <jvam90@gmail.com>
2022-07-11 22:06:57 -0300

    More primitive types

--- Types/Program.cs
+++ Types/Program.cs
@@ -13,6 +13,15 @@
             byte b = 0;
             Byte alsoB = 1;
 
+            sbyte sb = 1;
+            SByte alsoSb = 2;
+
+            float f = 0.0f;
+            Single alsoF = 0.0f;
+
+            double d = 3.14;
+            Double alsoD = 3.14;
+
             Console.WriteLine("Int32 == int");
             Console.WriteLine(age);
             Console.WriteLine(alsoAge);
@@ -22,6 +31,15 @@
             Console.WriteLine("Byte == byte");
             Console.WriteLine(b);
             Console.WriteLine(alsoB);
+            Console.WriteLine("SByte == sbyte");
+            Console.WriteLine(sb);
+            Console.WriteLine(alsoSb);
+            Console.WriteLine("Single == float");
+            Console.WriteLine(f);
+            Console.WriteLine(alsoF);
+            Console.WriteLine("Double == double");
+            Console.WriteLine(sb);
+            Console.WriteLine(alsoSb);
         }
     }
 }
